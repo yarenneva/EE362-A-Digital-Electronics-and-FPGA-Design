927eb4438b3194d9fcdd3b269fc386437fba8c1d
yarenneva <80396112+yarenneva@users.noreply.github.com>
2022-02-03 18:14:23 +0300

    Add files via upload

---
+++ VGA/VGAadjustSpeed/VGAwithSwitch.v
@@ -0,0 +1,17 @@
+module VGAwithSwitch(push,clk, rst, switch,	hsync, vsync, rgb);
+
+input [1:0] push;
+input clk, rst;
+input [2:0] switch;
+output hsync, vsync;
+output [2:0] rgb;
+
+wire video_on;
+wire [9:0] pixel_x, pixel_y;
+
+// instantiate vgaSync and pixelGeneration circuit
+vgaSync vgaSync(.clk(clk), .rst(rst), .hsync(hsync), .vsync(vsync), .video_on(video_on), .p_tick(), .pixel_x(pixel_x), .pixel_y(pixel_y));
+
+pixelGeneration pixelGeneration(.clk(clk),.rst(rst),.push(push),.switch(switch), .pixel_x(pixel_x), .pixel_y(pixel_y), .video_on(video_on), .rgb(rgb));
+
+endmodule
